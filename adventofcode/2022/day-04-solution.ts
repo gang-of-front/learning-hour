e191c1720ada987e218999736b7cdb9b0f34b505
Guilherme Mangabeira Gregio <guilherme.gregio@creditas.com.br>
2022-12-04 03:48:53 -0300

    feat(adventofcode): solved day 4 part 1

--- adventofcode/2022/day-04-solution.ts
+++ adventofcode/2022/day-04-solution.ts
@@ -2,10 +2,46 @@ import { readInput } from "./read-input";
 
 const rl = readInput("./day-04-input.txt");
 
+const generateRangeArr = ([min, max]: number[]) => {
+  const arr = [];
+  for (let i = min; i < max; i = i + 1) {
+    arr.push(i);
+  }
+  arr.push(max);
+
+  return arr;
+};
+
+const isFullyContains = (pairs: string[]) => {
+  const [pairOne, pairTwo] = pairs;
+
+  let sectionOne = pairOne.split("-").map(Number);
+  let sectionTwo = pairTwo.split("-").map(Number);
+
+  const firstRange = generateRangeArr(sectionOne);
+  const lastRange = generateRangeArr(sectionTwo);
+  const intersection = firstRange.filter((n) => lastRange.includes(n));
+
+  if (intersection.length === 0) {
+    return false;
+  }
+
+  const isFully =
+    JSON.stringify(intersection) === JSON.stringify(firstRange) ||
+    JSON.stringify(intersection) === JSON.stringify(lastRange);
+
+  return isFully;
+};
+
+let count = 0;
 rl.on("line", (line) => {
-  console.log(line);
+  const pairs = line.split(",");
+
+  if (isFullyContains(pairs)) {
+    count++;
+  }
 });
 
 rl.on("close", () => {
-  console.log({ answer: {} });
+  console.log({ answer: { count } });
 });
