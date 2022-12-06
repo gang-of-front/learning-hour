cfc10e1bac946fc86438a7279787045d2ccfadf9
Guilherme Mangabeira Gregio <guilherme.gregio@creditas.com.br>
2022-12-06 09:34:10 -0300

    refactor(adventofcode): day 06 to use shared helper

--- adventofcode/2022/day-06-solution.ts
+++ adventofcode/2022/day-06-solution.ts
@@ -2,33 +2,8 @@ import { readInput } from "./read-input";
 
 const rl = readInput("./day-06-input.txt");
 
-let part1: string[] = [];
-rl.on("line", (line) => {
-  part1 = line.split("").reduce((acc, item, index, arr) => {
-    const item1 = arr[index] || "";
-    const item2 = arr[index + 1] || "";
-    const item3 = arr[index + 2] || "";
-    const item4 = arr[index + 3] || "";
-
-    const uniq = new Set([item1, item2, item3, item4]);
-
-    if (
-      index + (1 % 4) &&
-      index <= arr.length - 4 &&
-      uniq.size === 4 &&
-      acc.length === 0
-    ) {
-      acc = [...uniq, String(index + 4)];
-    }
-
-    return acc;
-  }, [] as string[]);
-});
-
-const size = 14;
-let part2: string[] = [];
-rl.on("line", (line) => {
-  part2 = line.split("").reduce((acc, item, index, arr) => {
+const startOfMessageMarker = (line: string, size: number) => {
+  return line.split("").reduce((acc, _item, index, arr) => {
     const chunck = arr.slice(index, index + size);
 
     const uniq = new Set(chunck);
@@ -37,13 +12,23 @@ rl.on("line", (line) => {
       index + (1 % size) &&
       index <= arr.length - size &&
       uniq.size === size &&
-      acc.length === 0
+      acc === 0
     ) {
-      acc = [...uniq, String(index + size)];
+      acc = index + size;
     }
 
     return acc;
-  }, [] as string[]);
+  }, 0);
+};
+
+let part1: number;
+rl.on("line", (line) => {
+  part1 = startOfMessageMarker(line, 4);
+});
+
+let part2: number;
+rl.on("line", (line) => {
+  part2 = startOfMessageMarker(line, 14);
 });
 
 rl.on("close", () => {
