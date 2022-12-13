4d90412bc4d26c57add65d2066493e2ba3e0b71a
Guilherme Mangabeira Gregio <guilherme.gregio@creditas.com.br>
2022-12-13 01:50:50 -0300

    feat(adventofcode): solution day 10 part 2

--- adventofcode/2022/day-10-solution.ts
+++ adventofcode/2022/day-10-solution.ts
@@ -11,12 +11,26 @@ const addX = (value: number) => {
   registerX += value;
 };
 
+let draws: string[] = ["", "", "", "", "", "", ""];
+let spriteLine = 0;
+
 rl.on("line", (line) => {
   const command = line.split(" ");
 
   const cycles = command[0] === "addx" ? 2 : 1;
 
   for (let i = 1; i <= cycles; i++) {
+    const recycle = (clock - 1) % 40;
+    if (recycle === 0) {
+      spriteLine++;
+    }
+
+    if ([registerX - 1, registerX, registerX + 1].includes(recycle)) {
+      draws[spriteLine] += "#";
+    } else {
+      draws[spriteLine] += ".";
+    }
+
     if ((clock - 20) % 40 === 0) {
       signalStrengths += registerX * clock;
     }
@@ -30,5 +44,8 @@ rl.on("line", (line) => {
 });
 
 rl.on("close", () => {
-  console.log({ answer: { part1: signalStrengths } });
+  draws.forEach((row) => {
+    console.log(row);
+  });
+  console.log({ answer: { part1: signalStrengths, part2: {} } });
 });
